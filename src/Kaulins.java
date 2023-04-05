9c2230469d7269566f0b04a77b1eaee98660e62d
Moskovojs <rihards.moskovojs05@gmail.com>
2023-04-05 09:33:10 +0300

    commits

--- src/Kaulins.java
+++ src/Kaulins.java
@@ -14,8 +14,10 @@ public class Kaulins {
 	public static void main(String[] args) {
 		int reizes;
 		Scanner scan = new Scanner(System.in);
-		System.out.println("Cik reizes mest kauliņu?");
+		do {
+		System.out.println("Cik reizes mest kaulinu?");
 		reizes = scan.nextInt();
+		}while(reizes<1);
 		mestKaulinu(reizes);
 		scan.close();
 	}
